d4f2e252dad63309b78211c0f98af6332fdeb1ad
ilyasofficial1617 <ilyasofficial1617@gmail.com>
2019-11-25 22:22:40 +0700

    Konfigurasi email + Model User

--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,18 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'encryption' => 'tls',
+                'host' => 'smtp.gmail.com',
+                'port' => '587',
+
+                'username' => 'slaterbeepboop@gmail.com',
+                'password' => 'beepboopbeepboop',
+            ],
+        ],
     ],
 ];
--- common/models/User.php
+++ common/models/User.php
@@ -21,6 +21,7 @@ use yii\web\IdentityInterface;
  * @property integer $created_at
  * @property integer $updated_at
  * @property string $password write-only password
+ * @property integer $admin_level
  */
 class User extends ActiveRecord implements IdentityInterface
 {
@@ -28,6 +29,7 @@ class User extends ActiveRecord implements IdentityInterface
     const STATUS_INACTIVE = 9;
     const STATUS_ACTIVE = 10;
 
+    public $admin_level = 0;
 
     /**
      * {@inheritdoc}
@@ -55,6 +57,7 @@ class User extends ActiveRecord implements IdentityInterface
         return [
             ['status', 'default', 'value' => self::STATUS_INACTIVE],
             ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_INACTIVE, self::STATUS_DELETED]],
+            ['admin_level', 'default', 'value' => 0],
         ];
     }
 
